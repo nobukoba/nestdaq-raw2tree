2845fef321ef6c09151515b0e5fedf395eabe73b
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2025-09-08 11:02:04 +0900

    pla was added

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
-TARGET    = nestdaq-raw2tree-sglch
+TARGET1    = nestdaq-raw2tree-sglch
+TARGET2    = nestdaq-raw2tree-pla
 
 CXX       = g++
 CXXFLAGS  = $(shell root-config --cflags) -std=c++17 -Werror -Wall -O2
 LFLAGS    = 
 ROOTLIBS  = $(shell root-config --libs)
 
-all:	$(TARGET)
+all:	$(TARGET1) $(TARGET2)
 
-$(TARGET): $(TARGET).o
+$(TARGET1): $(TARGET1).o
+	$(CXX) $(LFLAGS) -o $@ $^ $(ROOTLIBS)
+$(TARGET2): $(TARGET2).o
 	$(CXX) $(LFLAGS) -o $@ $^ $(ROOTLIBS)
 %.o: %.cxx
 	$(CXX) $(CXXFLAGS) -c $<
 .PHONY : clean
 clean:
-	rm -rf $(TARGET) *.o *~
+	rm -rf $(TARGET1) $(TARGET2) *.o *~
--- nestdaq-raw2tree-pla.cxx
+++ nestdaq-raw2tree-pla.cxx
@@ -56,6 +56,43 @@ int read_tf (std::ifstream &ifs, uint64_t max_num_read_tf,
   return 0;
 }
 
+int fill_tree( std::map<int, Long64_t>& map_rawtdc,
+	       std::map<int, Long64_t>& map_rawtot,
+	       uint64_t* selectedChs,
+	       int64_t& hbfn0, Long64_t& rawhbfn, Long64_t& hbfn,
+	       Long64_t* rawtdc, Long64_t* rawtot,
+	       Double_t* tdc, Double_t*tot,
+	       TTree* tr){
+  static Long64_t prev_rawhbfn = 0;
+  static Long64_t prev_rawtdc  = 0;
+  if(map_rawtdc.count(selectedChs[0])==0){ return 0; }
+  if ((map_rawtdc[selectedChs[0]] == prev_rawtdc) &&
+      (rawhbfn == prev_rawhbfn)) {
+    return 0;
+  }
+  prev_rawtdc = map_rawtdc[selectedChs[0]];
+  prev_rawhbfn = rawhbfn;
+  
+  for (int ich = 0; ich < 9; ich++) {
+    uint64_t ch = selectedChs[ich];
+    if (map_rawtdc.count(ch) != 0){
+      rawtdc[ich] = map_rawtdc[ch];
+      rawtot[ich] = map_rawtot[ch];
+      tdc[ich] = hbfn * 524288.0 + rawtdc[ich] / 1024.;       // 1 hbf = 0.524288 msec, unit of the parameter "tdc" is nsec
+      tot[ich] = rawtdc[ich] / 1024.;                         // 1 LSB = 1 ns / 1024 ~ 0.9766 ps
+    }else{
+      rawtdc[ich] = -1000;
+      tdc[ich]    = -1000;
+      rawtot[ich] = -1000;
+      tot[ich]    = -1000;
+    }
+  }
+  tr->Fill();
+  map_rawtdc.clear();
+  map_rawtot.clear();
+  return 0;
+}
+
 int print_read_ratio(std::ifstream &ifs){
   static bool first_call_flag = true;
   static std::ifstream::pos_type fsize = 0;
@@ -98,15 +135,17 @@ int main(int argc, char* argv[]){
   TFile * of = TFile::Open(rootfile.c_str(),"RECREATE");
   std::cout << "ROOT file: "<< rootfile << std::endl;
   TTree *tr = new TTree("tr","tr");
-  Long64_t rawtdc, rawtot, rawhbfn, hbfn;
-  Double_t tdc;
+  Long64_t rawhbfn, hbfn;
+  Long64_t rawtdc[9], rawtot[9];
+  Double_t tdc[9], tot[9];
   std::map<int, Long64_t> map_rawtdc;
   std::map<int, Long64_t> map_rawtot;
-  tr->Branch("rawtdc",  &rawtdc,  "rawtdc/L");
-  tr->Branch("rawtot",  &rawtot,  "rawtot/L");
   tr->Branch("rawhbfn", &rawhbfn, "rawhbfn/L");
   tr->Branch("hbfn",    &hbfn,    "hbfn/L");
-  tr->Branch("tdc",     &tdc,     "tdc/D");
+  tr->Branch("rawtdc",  rawtdc,   "rawtdc[9]/L");
+  tr->Branch("rawtot",  rawtot,   "rawtot[9]/L");
+  tr->Branch("tdc",     tdc,      "tdc[9]/D");
+  tr->Branch("tot",     tot,      "tot[9]/D");
   
   FileSinkHeader::Header fileHeader;
   ifs.read((char*)&fileHeader,sizeof(fileHeader));
@@ -121,18 +160,11 @@ int main(int argc, char* argv[]){
   std::cout << "-----------------------------------" << std::endl;
   std::cout << "Then, starting analysis..." << std::endl;
   
-  uint64_t selectedFemId = 0xc0a802a9; // IP 192.168.2.169 was selected
-  uint64_t selectedCh    = 33;         // IP 192.168.2.169, ch33 was selected.
-  uint64_t currentFemId  = 0xffffffff;
-  uint64_t currentCh     = 0xffff;
+  uint64_t selectedChs[9] = {33,10,11,12,13,42,43,44,45}; // IP 192.168.2.169, ch33 was selected.
   
   int64_t  hbfn0                  = -1;
-  int64_t  hbfnPrev               = -1;
-  int64_t  rawtdcPrev             = -1;
   uint64_t hbfnCarryFlag          = 0;
-  uint64_t hit_counter            = 0;
-  uint64_t hit_counter_no_double  = 0;
-  //uint32_t currTimeFrameId = 0x1000000;
+  uint32_t currTimeFrameId        = 0x1000000;
   
   while( !sorted_time_frame_data.empty() ){
     print_read_ratio(ifs);
@@ -141,6 +173,7 @@ int main(int argc, char* argv[]){
     read_tf(ifs, max_num_read_tf, sorted_time_frame_data);
     char* ptr = sorted_time_frame_data.begin()->second.data();
     char* end = sorted_time_frame_data.begin()->second.data() + sorted_time_frame_data.begin()->second.size();
+    int32_t iSubTimeFrame = 0;
     while (ptr < end) {
       uint64_t magic = *reinterpret_cast<const uint64_t*>(ptr);
       switch (magic) {
@@ -150,9 +183,22 @@ int main(int argc, char* argv[]){
 	if (hbfn0 == -1) {
 	  hbfn0 = tfbHeader.timeFrameId;
 	}
+	if (currTimeFrameId == 0x1000000) {
+	  currTimeFrameId = tfbHeader.timeFrameId;
+	}else if (currTimeFrameId != tfbHeader.timeFrameId) {
+	  if (tfbHeader.timeFrameId < currTimeFrameId) {
+	    hbfnCarryFlag++;
+	  }
+	  currTimeFrameId = tfbHeader.timeFrameId;
+	}
 	//std::cout << "TimeFrameId: " << std::dec << tfbHeader.timeFrameId << std::hex << " 0x" << tfbHeader.timeFrameId << std::endl;
 	break;}
       case SubTimeFrame::MAGIC: {
+	if (iSubTimeFrame > 0) {
+	  fill_tree(map_rawtdc, map_rawtot, selectedChs,
+		    hbfn0, rawhbfn, hbfn, rawtdc, rawtot, tdc, tot, tr);
+	}
+	iSubTimeFrame++;
 	SubTimeFrame::Header stfHeader = *reinterpret_cast<const SubTimeFrame::Header*>(ptr);
 	ptr += sizeof(SubTimeFrame::Header);
 	unsigned int nword = (stfHeader.length - sizeof(stfHeader)) / 8;
@@ -160,45 +206,22 @@ int main(int argc, char* argv[]){
 	  AmQStrTdc::Data::Bits idata = *reinterpret_cast<const AmQStrTdc::Data::Bits*>(ptr);
 	  ptr += sizeof(AmQStrTdc::Data::Bits);
 	  if (idata.head == AmQStrTdc::Data::Heartbeat) {
-	    if ((stfHeader.femId == selectedFemId)
-	        && (currentFemId == selectedFemId)
-	        && (currentCh == selectedCh)){
-	      if ((hbfnPrev - idata.hbframe) > 0x10000) {
-	        hbfnCarryFlag++;
-	      }
-	      if ( (rawtdc != rawtdcPrev)
-		   || (idata.hbframe != hbfnPrev) ) {
-	        rawhbfn = idata.hbframe;
-	        hbfn = idata.hbframe + hbfnCarryFlag * 0x1000000 - hbfn0; // hbfn0: first heart beat frame number 
-	        tdc = hbfn * 524288.0 + rawtdc / 1024.;                  // 1 hbf = 0.524288 msec, unit of the parameter "tdc" is nsec
-	        tr->Fill();
-	        hit_counter_no_double++;
-	        //std::cout << "FemId: 0x" << std::hex << std::setw(8) << std::setfill('0') << stfHeader.femId << std::setfill(' ') << std::dec;
-	        //std::cout << ", hbfn: " << hbfn
-	        //	      << ", rawhbfn: " << rawhbfn
-	        //	      << ", rawtdc:  " << rawtdc
-	        //	      << ", rawtot:  " << rawtot
-	        //	      << ", hbfnCarryFlag:  " << hbfnCarryFlag
-	        //	      << ", tdc: " << tdc << std::endl;
-	      }
-	      rawtdcPrev   = rawtdc;
-	      hbfnPrev     = idata.hbframe;
-	      currentFemId = 0xffffffff;
-	      currentCh    = 0xffff;
-	    }
+	    rawhbfn = idata.hbframe;
+	    hbfn = idata.hbframe + hbfnCarryFlag * 0x1000000 - hbfn0; // hbfn0: first heart beat frame number 
+	    //std::cout << "FemId: 0x" << std::hex << std::setw(8) << std::setfill('0') << stfHeader.femId << std::setfill(' ') << std::dec;
+	    //std::cout << ", hbfn: " << hbfn
+	    //	      << ", rawhbfn: " << rawhbfn
+	    //	      << ", rawtdc:  " << rawtdc
+	    //	      << ", rawtot:  " << rawtot
+	    //	      << ", hbfnCarryFlag:  " << hbfnCarryFlag
+	    //	      << ", tdc: " << tdc << std::endl;
 	  }else if (idata.head == AmQStrTdc::Data::Data){
 	    if ( stfHeader.femType == 2 || stfHeader.femType == 5 ) { // HRTDC
-	      if ((stfHeader.femId == selectedFemId)
-		  && (idata.hrch == selectedCh)){
-		  rawtdc = idata.hrtdc;
-		  rawtot = idata.hrtot;
-		  hit_counter++;
+	      if (map_rawtdc.count(idata.hrch) == 0){
+		map_rawtdc[idata.hrch] = idata.hrtdc;
+	        map_rawtot[idata.hrch] = idata.hrtot;
 	      }
-	      currentFemId = stfHeader.femId;
-	      currentCh    = idata.hrch;
 	    }else if ( stfHeader.femType == 3 || stfHeader.femType == 6 ) { // LRTDC
-	      currentFemId = stfHeader.femId;
-	      currentCh = idata.ch;
 	    }
 	  }
 	}
@@ -217,12 +240,12 @@ int main(int argc, char* argv[]){
 	break;}
       }
     }
+    if (iSubTimeFrame > 0) {
+      fill_tree(map_rawtdc, map_rawtot, selectedChs,
+		hbfn0, rawhbfn, hbfn, rawtdc, rawtot, tdc, tot, tr);
+    }
   }
   tr->Write();
   of->Close();
-  std::cout << "For IP address: 0x" << std::hex << std::setw(8) << std::setfill('0') << selectedFemId << std::setfill(' ') << std::dec
-	    << ", ch: " << selectedCh << std::endl;
-  std::cout << "Hit count: " << hit_counter << std::endl;
-  std::cout << "Hit count (no double count): " << hit_counter_no_double << std::endl;
   return 0;
 }
